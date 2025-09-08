0b39d4d440b912615158f330c71d54b061b90631
ml11109 <leejhmalcolm@gmail.com>
2025-09-08 17:15:32 +0800

    Add support for dataset transformations in TestDataset
    Implemented optional data transformations with new classes (ToTensor, MulXTransform, AddXTransform). Updated TestDataset to integrate these transformations and modified its initialization to apply a transformation pipeline using torchvision's Compose. This enhances dataset preprocessing flexibility.

--- demos/data_loader.py
+++ demos/data_loader.py
@@ -1,24 +1,53 @@
 import torch
+import torchvision
 from torch.utils.data import Dataset, DataLoader
+import numpy as np
 import math
 
 
 class TestDataset(Dataset):
-    def __init__(self, x_data, y_data):
+    def __init__(self, x_data, y_data, transform=None):
         self.x_data = x_data
         self.y_data = y_data
         self.n_samples = x_data.shape[0]
+        self.transform = transform
 
     def __getitem__(self, index):
-        return self.x_data[index], self.y_data[index]
+        sample = self.x_data[index], self.y_data[index]
+        return self.transform(sample) if self.transform else sample
 
     def __len__(self):
         return self.n_samples
 
 
+class ToTensor:
+    def __call__(self, sample):
+        x, y = sample
+        return torch.tensor(x), torch.tensor(y)
+
+
+class MulXTransform:
+    def __init__(self, factor):
+        self.factor = factor
+
+    def __call__(self, sample):
+        x, y = sample
+        return x * self.factor, y
+
+
+class AddXTransform:
+    def __init__(self, factor):
+        self.factor = factor
+
+    def __call__(self, sample):
+        x, y = sample
+        return x + self.factor, y
+
+
 dataset = TestDataset(
-    x_data=torch.tensor([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]]),
-    y_data=torch.tensor([[1], [2], [3], [4], [5], [6]])
+    x_data=np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14]]),
+    y_data=np.array([[1], [2], [3], [4], [5], [6], [7]]),
+    transform=torchvision.transforms.Compose([ToTensor(), MulXTransform(factor=2), AddXTransform(factor=10)])
 )
 
 n_epochs = 10
